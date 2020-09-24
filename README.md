9a36836c95860be4f8ab0e8d2d67193041840441
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-09-23 21:01:47 -0700

    added readme

--- README.md
+++ README.md
@@ -1,3 +1,80 @@
-# Job Finder
+## Project-2 : Job Finder
 
-#
+Team:
+* Pratyusha     : Fontend & Backend 
+* Gauri Khandke : Backend & Backend 
+* Robeil        : Frontend          
+* Jason Kwong   : Backend & Backend 
+* Baldeep Kang  : Backend & Backend 
+
+
+* ![login image](https://uw-jobfinder.herokuapp.com/)
+* ![Sign up image](https://uw-jobfinder.herokuapp.com/signup)
+* ![Search job image](https://uw-jobfinder.herokuapp.com/members)
+* ![Post job image](https://uw-jobfinder.herokuapp.com/postjob.html)
+
+ # User Story
+
+This Website create to simplify finding a Job.
+
+
+
+
+## Tools Used:
+
+* HTML
+* CSS
+* JavaScript
+* GET and POST routes
+* JavaScript linter
+* Travis CI
+* Node and Express server
+* MySQL database with Sequelize ORM
+* Handlebars
+* bootstrap
+
+# User Criteria:
+
+GIVEN The glass is desktop or mobile,
+
+​WHEN The user interacts with the app
+
+THEN The pages are responsive.
+
+## To Job seekers
+
+GIVEN The user  lands on the page (login page),
+
+THEN THE user can  login or sign up if don't have exist account
+
+WHEN THE user sign up will direct to login page to login
+
+THEN THE user will direct to search job page and the searched result will display in the page.
+
+THE USER can logout
+
+## To Post Jobs
+
+GIVEN The user lands on the page (login page),
+
+THEN THE user can  login or sign up if don't have exist account
+
+WHEN THE user sign up will direct to login page to login
+
+THEN THE user will direct to post job page for posting new Jobs.
+
+THE USER can logout
+
+## Future Development:
+
+1 Ability to upload resume in order to get recognition by the recruiters.
+
+2 Create user portfolio.
+
+3 Able to get Notification from the recruiters.
+
+4 Shows who visit portfolio.
+
+
+## Deployed link on Heroku: https://uw-jobfinder.herokuapp.com/
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,37 +1,37 @@
--- Drop databse if not exists
--- DROP DATABASE IF EXISTS jobfinder_db;
+ Drop databse if not exists
+ DROP DATABASE IF EXISTS jobfinder_db;
 
--- CREATE DATABASE jobfinder_db;
+ CREATE DATABASE jobfinder_db;
 
--- use jobfinder_db;
+ use jobfinder_db;
 
--- create table User(
---     id integer auto_increment NOT NULL,
---     firstname VARCHAR(50),
---     lastname VARCHAR(50),
---     email VARCHAR(100),
---     password VARCHAR(20),
---     PRIMARY KEY(id)
---     -- time will be created at sequelize
--- );
+ create table User(
+     id integer auto_increment NOT NULL,
+     firstname VARCHAR(50),
+     lastname VARCHAR(50),
+     email VARCHAR(100),
+     password VARCHAR(20),
+     PRIMARY KEY(id)
+      time will be created at sequelize
+ );
 
 
--- CREATE TABLE Jobs(
---     id  integer auto_increment NOT NULL,
---     role VARCHAR(30) NOT NULL,
---     description VARCHAR(1000) NOT NULL,
---     technology VARCHAR(50) NOT NULL,
---     company VARCHAR(100) NOT NULL,
---     jobtype VARCHAR(50) NOT NULL,
---     salary DECIMAL,
---     joblocation VARCHAR(50) NOT NULL,
---     contact VARCHAR(70) NOT NULL,
---     --date posted will be created at sequelize
---     userId INTEGER,
---     -- user in jobs table
---     CONSTRAINT fk_users FOREIGN KEY(userId)
---     REFERENCES user(id)
---         ON UPDATE CASCADE
---         ON DELETE CASCADE,
---     PRIMARY KEY(id)
--- );
+ CREATE TABLE Jobs(
+     id  integer auto_increment NOT NULL,
+     role VARCHAR(30) NOT NULL,
+     description VARCHAR(1000) NOT NULL,
+     technology VARCHAR(50) NOT NULL,
+     company VARCHAR(100) NOT NULL,
+     jobtype VARCHAR(50) NOT NULL,
+     salary DECIMAL,
+     joblocation VARCHAR(50) NOT NULL,
+     contact VARCHAR(70) NOT NULL,
+     date posted will be created at sequelize
+     userId INTEGER,
+      user in jobs table
+     CONSTRAINT fk_users FOREIGN KEY(userId)
+     REFERENCES user(id)
+         ON UPDATE CASCADE
+         ON DELETE CASCADE,
+     PRIMARY KEY(id)
+ );
