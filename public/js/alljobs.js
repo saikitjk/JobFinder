dce8285a38f16422992204165f1f8ffd148efa23
Gauri Khandke <khandkegauri@gmail.com>
2020-09-24 18:37:11 -0700

    Created handlebars page for alljobs

---
+++ public/js/alljobs.js
@@ -0,0 +1,24 @@
+// // /* eslint-disable prefer-const */
+// $(() => {
+//   //   console.log("in findall jobs function");
+//   //   let jobs = [];
+//   getJobs();
+//   //   // function initializeJobs() {
+//   //   //   console.log(jobs);
+//   //   //   $alljobs.empty();
+//   //   //   const jobsToAdd = [];
+//   //   //   for (let i = 0; i < jobs.length; i++) {
+//   //   //     jobsToAdd.push(jobs[i]);
+//   //   //   }
+//   //   //   $alljobs.prepend(jobsToAdd);
+//   //   // }
+
+//  // function getJobs() {
+//  //   $.get("/api/jobs", jobsData => {
+//   //    console.log(jobsData);
+//       // jobs.push(jobsData);
+//       // console.log("jobs" + jobs);
+
+//   //  });
+//  // }
+// });
